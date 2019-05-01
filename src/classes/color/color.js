53d2fb47692645c70d57105a3dfff0f6b3060b7c
Jake Jenne <jake.jenne@nuvi.com>
2019-05-01 11:15:19 -0600

    color

--- src/classes/color/color.js
+++ src/classes/color/color.js
@@ -1,12 +1,72 @@
+const hex_r = /#([a-f0-9]{6})/i;
+const rgb_r = /rgba?\(([0-9]+)\s*,\s*([0-9]+)\s*,\s*([0-9]+)\s*(,\s*([0-9.]+)\s*)?\)/;
+
 class Color {
-  constructor (r, g, b, a = 1) {
+  static blendColors(colorA, weightA, colorB, weightB) {
+    let a = colorA;
+    if (typeof(colorA) === 'string') {
+      a = new Color(colorA);
+    }
+    let b = colorB;
+    if (typeof(colorB) === 'string') {
+      b = new Color(colorB);
+    }
+    const wa = weightA / (weightA + weightB);
+    const wb = weightB / (weightA + weightB);
+    return new Color(a.r * wa + b.r * wb,
+                     a.g * wa + b.g * wb,
+                     a.b * wa + b.b * wb,
+                     a.a * wa + b.a * wb);
+  }
+
+  constructor(...args) {
+    if (args.length === 1 && typeof (args[0]) === 'string') {
+      this.parse(args[0]);
+    } else if (args.length === 3 || args.length === 4 && typeof (args[0]) === 'number') {
+      this.fromRGBA(...args);
+    }
+  }
+
+  multiplyAlpha(value) {
+    const pct = parseFloat(value, 10) || 1;
+    this.a *= pct;
+    return this;
+  }
+
+  parse(str) {
+    if (hex_r.test(str)) {
+      this.parseHex(str);
+    } else if (rgb_r.test(str)) {
+      this.parseRGB(str);
+    }
+  }
+
+  parseHex(str) {
+    const match = str.match(hex_r);
+    const base16 = match[1];
+    const r = base16.substring(0, 2);
+    const g = base16.substring(2, 4);
+    const b = base16.substring(4, 6);
+    this.fromRGBA(parseInt(r, 16), parseInt(g, 16), parseInt(b, 16));
+  }
+
+  parseRGB(str) {
+    const match = str.match(rgb_r);
+    const r = match[1];
+    const g = match[2];
+    const b = match[3];
+    const a = match[5] || 1;
+    this.fromRGBA(parseInt(r, 10), parseInt(g, 10), parseInt(b, 10), parseFloat(a, 10));
+  }
+
+  fromRGBA(r, g, b, a = 1) {
     this.r = r;
     this.g = g;
     this.b = b;
     this.a = a;
   }
 
-  toString () {
+  toString() {
     const r = Math.floor(this.r);
     const g = Math.floor(this.g);
     const b = Math.floor(this.b);
@@ -14,4 +74,4 @@ class Color {
   }
 }
 
-module.exports = Color;
+export default Color;
